e711fcbc8f97e1ec97cc8164f5a94ef185eb37ff
ley <leigh00545@gmail.com>
2026-08-14 18:24:23 +0800

    slingshot bullet setup

--- Assets/Scripts/mouseDetector.cs
+++ Assets/Scripts/mouseDetector.cs
@@ -3,15 +3,51 @@ using UnityEngine;
 public class mouseDetector : MonoBehaviour
 {
     [SerializeField] Vector2 currentMousePosition;
+
+    Vector2 startingPoint;
+    Vector2 direction;
+
+    Rigidbody2D rb2d;
+
+    [SerializeField] float force;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startingPoint = transform.position;
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDown()
+    {
+        startingPoint = transform.position;
+    }
+
+    private void OnMouseDrag()
+    {
+        if(Vector2.Distance(currentMousePosition, startingPoint) < 3)
+        {
+            transform.position = currentMousePosition;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        rb2d.gravityScale = 1;
+        direction = startingPoint - currentMousePosition;
+        rb2d.linearVelocity = direction * force;
+        Invoke("resetPosition", 3f);
+    }
+
+    void resetPosition()
+    {
+        rb2d.gravityScale = 0;
+        transform.position = startingPoint;
+        rb2d.linearVelocity = Vector2.zero;
     }
 }
