a417840b49f2ceb6a8ea0f87ebb3eb4157931e1d
ley <leigh00545@gmail.com>
2026-08-26 02:22:15 +0800

    Final: exit script

---
+++ Assets/Scripts/UI/exit.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class exit : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
